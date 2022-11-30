b740441885f397fbb57f3867b8f4224c9b3e5f22
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-12-01 01:46:05 +0300

    connect service to todo list

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,14 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ITodoItem } from 'src/app/services/todo-list.service';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent {
   constructor() {}
 
-  @Input() todoItem!: ITodoItem;
-  // @Input() fromTodoListMetods!: Function;
+  isHideChangeStatuses = true;
+
+  private _todoItem!: ITodoItem;
+
+  @Input() set todoItem(todoItem: ITodoItem) {
+    this._todoItem = todoItem;
+  }
+  get todoItem(): ITodoItem {
+    return this._todoItem;
+  }
+
+  @Output()
+  public handleClickDelete = new EventEmitter();
+
+  @Output()
+  public handleClickChangeStatus = new EventEmitter();
+
+  changeStatus(id: number, status: string) {
+    this.handleClickChangeStatus.emit({id, status});
+
+    this.isHideChangeStatuses = !this.isHideChangeStatuses;
+  } 
 }
