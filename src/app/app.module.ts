7a7ee9bf5a52ecac5fde605333fe65429ac0ca17
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-12-01 05:12:03 +0300

    add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { CreateElemComponent } from './components/create-elem/create-elem.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoItemComponent,
     TodoListComponent,
-    CreateElemComponent
+    CreateElemComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
