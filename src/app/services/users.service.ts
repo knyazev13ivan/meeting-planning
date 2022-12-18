57e07e16ec66c8bb9cf8659a48a6b78db15e9521
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-12-18 19:25:16 +0300

    add user service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,8 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import {
+  IAddRoleDto,
+  ICreateUserDto,
+  IUpdateRoleDto,
+  IUser,
+} from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor() {}
+  usersUrl: string = `${environment.backendOrigin}/user`;
+  users: IUser[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<IUser[]> {
+    return this.http
+      .get<IUser[]>(this.usersUrl)
+      .pipe(tap((users) => (this.users = users)));
+  }
+
+  addRole(role: IAddRoleDto) {
+    this.http.put(`${this.usersUrl}/role`, role);
+  }
+
+  getRoles(roles: IUpdateRoleDto): Observable<IUpdateRoleDto> {
+    return this.http.post<IUpdateRoleDto>(`${this.usersUrl}/role`, roles);
+  }
+
+  updateUser(user: ICreateUserDto, id: number) {
+    this.http.put(`${this.usersUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number) {
+    this.http.delete(`${this.usersUrl}/${id}`);
+  }
 }
